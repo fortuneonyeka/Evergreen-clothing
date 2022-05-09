f25a053fab1f77619990f0733ede630fc3164a62
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-05-09 21:24:49 +0100

    add category image

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -4,32 +4,37 @@ import './categories.styles.scss'
 const Categories = () => {
   const categories = [
     {
-      title:"Hats",
-      id:1
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      title:"Sneakers",
-      id:2
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      title:"Jackets",
-      id:3
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      title:"Men's",
-      id:4
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      title:"Women's",
-      id:5
-    },
-
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
   ]
+  
   return (
-    <div className="categories-container">
-      {categories.map(({title ,id}) => (
+    <div className="categories-container" style={{marginTop:"150px"}}>
+      {categories.map(({title ,id,imageUrl}) => (
         <div key={id} className="category-container">
-        <div className="background-image" />
+        <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className="category-body-container">
           <h2>{title}</h2>
           <p>Shop Now</p>
